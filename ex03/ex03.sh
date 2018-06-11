ec9f9bbd269dd989330d6e76913c7a494dccad01
MaRk0pOl0kEk <anderhil1998@gmail.com>
2018-06-11 13:13:06 +0300

    worked ex03.sh

--- ex03/ex03.sh
+++ ex03/ex03.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-if [ "$#" == 4 ]; then
-  for i in 2 3 4;
-  do
-    echo "$i" $(grep -o $i $1 | wc -l)
-    grep -n $i $1 | cut --delimiter=':' -f 1
-  done
+if [ "$#" == 4 ];
+then
+  echo "$2" $(grep -o $2 $1 | wc -l)
+  grep -n $2 $1 | cut --delimiter=':' -f 1
+
+  echo "$3" $(grep -o $3 $1 | wc -l)
+  grep -n $3 $1 | cut --delimiter=':' -f 1
+
+  echo "$4" $(grep -o $4 $1 | wc -l)
+  grep -n $4 $1 | cut --delimiter=':' -f 1
 else
   exit 1
 fi
